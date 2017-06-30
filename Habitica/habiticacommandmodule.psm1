0ef3fcc32c3850efcae5ffe6becfec0612003e1c
David Knodel <dknodel@appriver.corp>
2017-06-30 08:28:56 -0500

    going home; committing current progress (data get)

--- Habitica/habiticacommandmodule.psm1
+++ Habitica/habiticacommandmodule.psm1
@@ -1,6 +1,7 @@
-# Set-RandomHabiticaPet v1.0
-# 04/04/2016
+# Habitica Command Module 0.1
+# 06/30/2017
 
+Function Get-HabiticaUserData {
 Param(
     [Parameter(ParameterSetName='ManualGuids',Position=0,Mandatory=$true)][string]$userguid,
     [Parameter(ParameterSetName='ManualGuids',Position=1,Mandatory=$true)][string]$keyguid#,
@@ -13,29 +14,11 @@ $headers = @{'x-api-user' = $userguid;'x-api-key' = $keyguid}
 
 #Habitica API URLs
 [string]$urlUserData = 'https://habitica.com/api/v3/user'
-[string]$urlEquipPet = 'https://habitica.com/api/v2/user/inventory/equip/pet/'
-
-#I saw no way in the API to grab your current inventory, but I did see that you can get your inventory when you set something
-#So the first part is when I grab whatever pet you currently have, then set it again
 
 #grabbing your current user data
 $UserData = Invoke-RestMethod -Uri $urlUserData -Headers $headers -Method GET
-[string]$currentPet = $UserData.items.currentPet
 
-#sometimes the API call fails, so you need this if statement so you don't set an empty pet 
-if ($currentPet) {
-    [string]$urlInventory = $urlEquipPet + $currentPet
-    
-    #setting the same pet and capturing the results
-    $allInventory = Invoke-RestMethod -Uri $urlInventory -Headers $headers -Method POST
-    
-    #geting the list of pets
-    $allPets = Get-Member -InputObject $allInventory.pets | where {$_.MemberType -eq "NoteProperty"}
+return $UserData
+}
 
-    #grabbing a random pet
-    $newPet = $allPets | Get-Random
-    [string]$urlNewPet = $urlEquipPet + $newPet.Name
-    
-    #setting the new pet
-    Invoke-RestMethod -Uri $urlNewPet -Headers $headers -Method POST
-} 
+Get-Member -inputobject $userdata.data.items.hatchingpotions
